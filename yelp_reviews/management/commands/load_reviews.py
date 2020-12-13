0d57a91a5af6eebe1c9c52b679d196e2e78aa2c0
dnmaster1 <dhimant.master@gmail.com>
2020-12-13 11:27:00 -0500

    pending pset 5 and test cases

--- yelp_reviews/management/commands/load_reviews.py
+++ yelp_reviews/management/commands/load_reviews.py
@@ -20,45 +20,22 @@ class Command(BaseCommand):
 
         filepaths = [ path + f for f in listdir(path) if f.endswith('.csv')]
         yr = pd.concat(map(pd.read_csv, filepaths))
-        # yr = pd.read_csv(path+'00.csv')
         numcols  = ["funny","cool","useful","stars"]
-        # yr['date'] = yr['date'].astype('datetime64[ns]')
-        # yr.set_index('review_id',drop=True)
-        # #yr.reset_index(drop=True, inplace=True)
-
-        # yr = yr.drop(['user_id','business_id','text','decade','text_length'],axis=1)
-        # yr[numcols] = yr[numcols].fillna(0)
-        # yr[numcols] = yr[numcols].astype(int)
-        # print(yr.head())
-
-
         yr.set_index('review_id', drop=True)
         print(yr.dtypes)
         yr[numcols] = yr[numcols].fillna(0)
         yr.dropna()
         yr = yr[yr['user_id'] != None]
         yr = yr[yr['review_id'].str.len() == 22]
-        # yr['funny'] = pd.to_numeric(yr['funny'], errors='coerce')
-        # yr = yr.dropna(subset=['funny'])
-        # yr['useful'] = pd.to_numeric(yr['useful'], errors='coerce')
-        # yr = yr.dropna(subset=['useful'])
-        # yr['cool'] = pd.to_numeric(yr['cool'], errors='coerce')
-        # yr = yr.dropna(subset=['cool'])
-
         yr['date'] = yr['date'].astype('datetime64[ns]')
         yr[numcols] = yr[numcols].astype(int)
-        #yr.to_csv("yr.csv", sep=',')
-        print(yr['review_id'].count())
         yr_sum = yr.groupby( ['date'] )["stars", "useful", "funny", "cool"].apply(lambda l: l.sum() )
-
         yr_sum["count"] = yr.date.value_counts(dropna=True)
         yr_sum = yr_sum.reset_index()
         yr_sum[numcols] = yr_sum[numcols].astype(int)
         yr_sum.dropna()
         yr_sum.to_csv("yr_sum.csv", index=False, sep=',', encoding='utf-8')
         yr_sum.dropna()
-
-
 
         DimDate.objects.all().delete()
         for index, row in yr_sum.iterrows():
@@ -67,20 +44,15 @@ class Command(BaseCommand):
             model.save()
         print(DimDate.objects.all()[0].date)
 
-
         FactReview.objects.all().delete()
-
 
         for index, row in yr_sum.iterrows():
             model = FactReview()
-            #print(DimDate.objects.get(date=row['date']))
             model.date = DimDate.objects.get(date=row['date'])
             model.count = row['count']
-            #print(type(row['count']), row['stars'])
             model.stars = row['stars']
             model.stars = row['funny']
             model.useful = row['useful']
             model.cool = row['cool']
             model.save()
 
-        print("My stars are", FactReview.objects.all()[0])
